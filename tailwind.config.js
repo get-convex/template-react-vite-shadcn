e362384d0c4b2da9a9dc7274c71f24e228fcaa3a
Michal Srb <srb@convex.dev>
2024-05-16 19:32:53 +0200

    Fix Tailwind config for dark mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,13 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  darkMode: ["class"],
+export default {
+  darkMode: "selector",
   content: [
     "./pages/**/*.{ts,tsx}",
     "./components/**/*.{ts,tsx}",
     "./app/**/*.{ts,tsx}",
     "./src/**/*.{ts,tsx}",
   ],
+  safelist: ["dark"],
   prefix: "",
   theme: {
     container: {
