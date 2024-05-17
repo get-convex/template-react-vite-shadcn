d838197f33042b17e523d41dd1c0adf4302c7bb0
Michal Srb <srb@convex.dev>
2024-05-17 14:57:47 +0200

    Add chat

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,16 @@
+import { Chat } from "@/Chat/Chat";
+import { ChatIntro } from "@/Chat/ChatIntro";
+import { randomName } from "@/Chat/randomName";
 import { Layout } from "@/Layout";
+import { UserMenu } from "@/components/UserMenu";
+import { useState } from "react";
 
 export default function App() {
-  return <Layout>Hello world</Layout>;
+  const [viewer] = useState(randomName());
+  return (
+    <Layout menu={<UserMenu>{viewer}</UserMenu>}>
+      <ChatIntro />
+      <Chat viewer={viewer} />
+    </Layout>
+  );
 }
